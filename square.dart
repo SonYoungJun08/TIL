6e9e5ccd21c5ca02514cb30851a2f046955ef32c
SonYoungJun08 <syjc0809@gmail.com>
2025-04-10 16:41:05 +0900

    네모 과제1

---
+++ square.dart
@@ -0,0 +1,66 @@
+
+void main() {
+  var n = 10;
+  print("  ");
+  for (var y = 0; y < n; y++) {
+    var line = "";
+    for (var x = 0; x < n; x++) {
+      line += "=";
+    }
+    print(line);
+  }
+  print("  ");
+  for (var y = 0; y < n; y++) {
+    var line = "";
+    for (var x = 0; x < n; x++) {
+      if (x == 0 || y == 0 || x == n - 1 || y == n - 1) {
+        line += "=";
+      } else {
+        line += " ";
+      }
+    }
+    print(line);
+  }
+  print("  ");
+  for (var y = 0; y < n; y++) {
+    var line = "";
+    for (var x = 0; x < n; x++) {
+      if (x == 0 || y == 0 || x == n - 1 || y == n - 1) {
+        line += "=";
+      } else if (x + y == n - 1) {
+        line += "=";
+      } else {
+        line += " ";
+      }
+    }
+    print(line);
+  }
+  print("  ");
+  for (var y = 0; y < n; y++) {
+    var line = "";
+    for (var x = 0; x < n; x++) {
+      if (x == 0 || y == 0 || x == n - 1 || y == n - 1) {
+        line += "=";
+      } else if (x == y) {
+        line += "=";
+      } else {
+        line += " ";
+      }
+    }
+    print(line);
+  }
+  print("  ");
+  for (var y = 0; y < n; y++) {
+    var line = "";
+    for (var x = 0; x < n; x++) {
+      if (x == 0 || y == 0 || x == n - 1 || y == n - 1) {
+        line += "=";
+      } else if (x == y || x + y == n - 1) {
+        line += "=";
+      } else {
+        line += " ";
+      }
+    }
+    print(line);
+  }
+}

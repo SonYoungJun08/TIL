e0e4d48361260ef8283ee9d9766630140a358cb3
SonYoungJun08 <syjc0809@gmail.com>
2025-04-10 16:39:44 +0900

    Delete lib directory

--- lib/date_day.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  var input = '2025-03-25';
-  var output = DateTime.parse(input);
-  var weekday = output.weekday;
-
-  var weekdays = ['월', '화', '수', '목', '금', '토', '일'];
-
-  print(weekdays[weekday - 1]);
-}

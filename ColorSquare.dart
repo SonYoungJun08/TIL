120bbc43267cc1a5729fe6484af5623df55707f6
SonYoungJun08 <syjc0809@gmail.com>
2025-09-01 13:43:14 +0900

    Update ColorSquare.dart

--- ColorSquare.dart
+++ ColorSquare.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey[300],
